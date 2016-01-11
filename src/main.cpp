de69de7dfacab6a0e29f88c06d02f87e161e5c12
Michael Spencer <sonrisesoftware@gmail.com>
2016-01-10 20:29:26 -0600

    Switch to CMake and add a desktop file and appdata for Linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,42 @@
 /*
-* Liri Calculator - A calculator application for Papyros
-* Copyright (C) 2015 Pierre Jacquier
-* http://pierre-jacquier.com
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Liri Calculator - A calculator application for Papyros
+ * Copyright (C) 2015 Pierre Jacquier (ttp://pierre-jacquier.com)
+ * Copyright (C) 2016 Michael Spencer <sonrisesoftware@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#ifndef QT_NO_WIDGETS
-#include <QtWidgets/QApplication>
-typedef QApplication Application;
-#else
 #include <QtGui/QGuiApplication>
-typedef QGuiApplication Application;
-#endif
 #include <QtQml/QQmlApplicationEngine>
 #include <QtQml/QQmlContext>
 
-
 int main(int argc, char **argv)
 {
-        Application app(argc, argv);
+        QGuiApplication app(argc, argv);
 
-        app.setOrganizationName("Pierre Jacquier");
-        app.setOrganizationDomain("github.com/pierremtb");
-        app.setApplicationName("liri-calculator");
-        QQmlApplicationEngine appEngine;
-        appEngine.load(QUrl("qrc:/qml/main.qml"));
-        QMetaObject::invokeMethod(appEngine.rootObjects().first(), "load");
+        app.setOrganizationName("Liri Project");
+        app.setOrganizationDomain("liriproject.me");
+        app.setApplicationName("Liri Calculator");
+
+        #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
+            app.setDesktopFileName("me.liriproject.Calculator.desktop");
+        #endif
+
+        // Set the X11 WML_CLASS so X11 desktops can find the desktop file
+        qputenv("RESOURCE_NAME", "me.liriproject.Calculator");
+
+        QQmlApplicationEngine engine(QUrl("qrc:/qml/main.qml"));
 
         return app.exec();
 }
